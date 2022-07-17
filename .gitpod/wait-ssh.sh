476f039e48677d4bf80770c79d6e9e5b35afc895
Ashish Patil <67854138+code2k13@users.noreply.github.com>
2022-07-17 13:34:34 +0530

    removed function declaration

--- .gitpod/wait-ssh.sh
+++ .gitpod/wait-ssh.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-function waitssh() {
-  while ! nc -z 127.0.0.1 2222; do   
-    sleep 0.1
-  done
-  ./ssh.sh "whoami" &>/dev/null
-  if [ $? -ne 0 ]; then
-    sleep 1
-    waitssh
-  fi
-}
+while ! nc -z 127.0.0.1 2222; do   
+  sleep 0.1
+done
+./ssh.sh "whoami" &>/dev/null
+if [ $? -ne 0 ]; then
+  sleep 1
+  waitssh
+fi
